4ed7c84650ddc430ed3b68e5c28def49459c4b71
savvylee11 <savvystephens@gmail.com>
2017-08-13 20:21:48 -0400

    created a child theme

--- wp-content/themes/childthemebusboys/functions.php
+++ wp-content/themes/childthemebusboys/functions.php
@@ -1,4 +1,5 @@
 <?php
+// created a child theme for my final project template 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 function enqueue_child_theme_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
